f4ef31b8f8eb4c85fff09a9e0d87055c6d19a99a
robikshrestha <robikshrestha@gmail.com>
2016-12-30 13:08:12 +0545

    Add a profile named "job"

--- src/main/java/com/erobic/springit/jobs/SchedulerDemoJob.java
+++ src/main/java/com/erobic/springit/jobs/SchedulerDemoJob.java
@@ -2,6 +2,7 @@ package com.erobic.springit.jobs;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,15 +11,16 @@ import org.springframework.stereotype.Component;
  * Created by robik on 12/30/16.
  */
 @Component
-public class ResearchResultJob {
+@Profile("job")
+public class SchedulerDemoJob {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Scheduled(cron = "0,30 * * * * *")
+    @Scheduled(cron = "${schedulerDemoJob.cron}")
     public void cronJob() {
         logger.info("Publishing new research results...");
     }
 
-    @Scheduled(fixedDelay = 10000l)
+    @Scheduled(fixedDelayString = "${schedulerDemoJob.fixedDelayString}")
     public void fixedDelayJob() throws InterruptedException {
         logger.info("Delayed by 10 seconds, sleeping for 20 seconds");
         Thread.sleep(20000l);
